401a8cb72fc269121438c842cb046cf26fcfb5e6
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-08 01:30:01 +0500

    Golang patners App

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+
+const uri = "mongodb+srv://root:1234@cluster0.ik76ncs.mongodb.net/?retryWrites=true&w=majority"
+
 func main() {
-	println("Hello, World!")
 }
