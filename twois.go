15cd15ec0e09e18fb2b2edc8818cac10fd702904
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-09 19:18:36 +0500

    Golang patners App

--- twois.go
+++ twois.go
@@ -1,33 +1,34 @@
 package main
 
-import (
-	"fmt"
-	"net/smtp"
-)
+// send email to sms
+// import (
+// 	"fmt"
+// 	"net/smtp"
+// )
 
-func sendMailSimple() {
-	auth :=smtp.PlainAuth(
-		"",
-		"dev.dilshodjon@gmail.com",
-		"soxjmnnrefcncvix",
-		"smtp.gmail.com",
-	)
+// func sendMailSimple() {
+// 	auth :=smtp.PlainAuth(
+// 		"",
+// 		"dev.dilshodjon@gmail.com",
+// 		"soxjmnnrefcncvix",
+// 		"smtp.gmail.com",
+// 	)
 
-	msg := "subject: Hello\n\nHello, this is a test email"
+// 	msg := "subject: massage send code"
 
-	err :=smtp.SendMail(
-		"smtp.gmail.com:587",
-		auth,
-		"doimjonovasadbek1002@gmail.com",
-		[]string{"doimjonovasadbek1002@gmail.com"},
-		[]byte(msg),
-	) 
-	if err != nil {
-		fmt.Println(err)
-	}
+// 	err :=smtp.SendMail(
+// 		"smtp.gmail.com:587",
+// 		auth,
+// 		"doimjonovasadbek1002@gmail.com",
+// 		[]string{"doimjonovasadbek1002@gmail.com"},
+// 		[]byte(msg),
+// 	) 
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
 
-}
+// }
 
-func main() {
-	sendMailSimple()
-}
+// func main() {
+// 	sendMailSimple()
+// }
