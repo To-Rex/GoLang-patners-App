45997f61cc11aa4c7aea86869a95c75e8b2757e5
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-11 00:00:28 +0500

    Golang patners App

--- smssend.go
+++ smssend.go
@@ -9,20 +9,31 @@ import (
 	  twilioApi "github.com/twilio/twilio-go/rest/api/v2010"
   )
 
-  func main() {
-	client := twilio.NewRestClient("ACc7e0d8c4e4b4f0e2c7f3b9d2c7b0e3b3", "e3e8d0e1c1c6e1a2b3d8b2b3c4d5e6f7")
+func main() {
+	// Your Account SID from twilio.com/console
+	accountSid := "ACd7d3e0b9c9a9a1a1d7d3e0b9c9a9a1a1"
+	// Your Auth Token from twilio.com/console
+	authToken := "your_auth_token"
+
+	// Twilio client
+	client := twilio.NewClient(accountSid, authToken, nil)
+
+	// Create message
 	params := &twilioApi.MessageCreateParams{
-		Body: "Hello from Twilio!",
-		To:   "+998909999999",
-		From: "+12058999999",
+		Body: "Hello from Go!",
+		To:   "+12345678901",
+		From: "+12345678901",
 	}
 
-	resp, err := client.Messages.Create(params)
+	// Send message
+	message, err := client.Messages.Create(params)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println(resp.Sid)
-  }
+	// Print message
+	json, _ := json.MarshalIndent(message, "", "  ")
+	fmt.Println(string(json))
+}
 
 
